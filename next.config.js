097a901d3ec7c9a50c7f91a7ce78fa9b7bc658e3
juan-uribe <juanuribe.dev@gmail.com>
2026-01-20 12:11:59 -0600

    Updated Node Version

--- next.config.js
+++ next.config.js
@@ -2,9 +2,15 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: [
-      "ichwtlkazihzvtpmxbnw.supabase.co",
-      "supabasekong-y4cw8s8o0k00wk8c8ko80scs.40.233.12.170.sslip.io",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ichwtlkazihzvtpmxbnw.supabase.co",
+      },
+      {
+        protocol: "http",
+        hostname: "supabasekong-y4cw8s8o0k00wk8c8ko80scs.40.233.12.170.sslip.io",
+      },
     ],
   },
   async rewrites() {
